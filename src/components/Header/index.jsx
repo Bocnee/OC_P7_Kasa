f5477bf120eaa7cf21b4a2e7c2e67cd4e98ab9ad
Océane Burel <oceanebrl.pro@gmail.com>
2023-02-28 14:20:50 +0100

    change styles & organisation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,26 @@
 import { NavLink, useLocation } from 'react-router-dom';
-import headerClasses from '../../styles/components/header.module.scss';
+import classes from '../../styles/components/header.module.scss';
 import logo from '../../assets/logo.svg';
 
 function Header() {
    const pathname = useLocation().pathname;
    return (
-      <header className={headerClasses.headerWrap}>
-         <div className={headerClasses.headerFlex}>
-            <h1 className={headerClasses.logoWrap}>
+      <header className={classes.headerWrap}>
+         <div className={classes.headerFlex}>
+            <h1 className={classes.logoWrap}>
                <img
                   draggable="false"
-                  className={headerClasses.logoWrap__logo}
+                  className={classes.logoWrap__logo}
                   src={logo}
                   alt="Logo de Kasa, Kasa écrit en toute lettre avec une maison pour remplacer le premier A"
                />
             </h1>
-            <nav className={headerClasses.navWrap}>
+            <nav className={classes.navWrap}>
                <NavLink
                   className={
                      pathname === '/'
-                        ? headerClasses.navWrap__item__active
-                        : headerClasses.navWrap__item
+                        ? `${classes.navWrap__item} ${classes.navWrap__item__active}`
+                        : classes.navWrap__item
                   }
                   to="/">
                   Accueil
@@ -28,8 +28,8 @@ function Header() {
                <NavLink
                   className={
                      pathname === '/about'
-                        ? headerClasses.navWrap__item__active
-                        : headerClasses.navWrap__item
+                        ? `${classes.navWrap__item} ${classes.navWrap__item__active}`
+                        : classes.navWrap__item
                   }
                   to="/about">
                   A Propos
--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,6 +1,8 @@
+import pages from '../../styles/utils/parts/pagesWrap.module.scss';
+
 function NotFound() {
    return (
-      <div>
+      <div className={pages.wrap}>
          <h1>Erreur 404.</h1>
       </div>
    );
