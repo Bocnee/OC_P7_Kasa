339ccac059c92787a605327f04713865b2cf15e6
Océane Burel <oceanebrl.pro@gmail.com>
2023-03-01 13:02:08 +0100

    Correction Banner Home Page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,5 @@
 import pages from '../../styles/utils/parts/pagesWrap.module.scss';
 import styles from '../../styles/pages/home.module.scss';
-import styleBanner from '../../styles/utils/parts/banners.module.scss';
 
 import { logementsList } from '../../datas/logementsList';
 import Card from '../../components/Card';
@@ -10,11 +9,11 @@ import homeImg from '../../assets/accueilImg.png';
 function Home() {
    return (
       <div className={pages.wrap}>
-         <div className={`${styleBanner.banner} ${styles.banner}`}>
+         <div className={styles.banner}>
             <img
                src={homeImg}
                alt="fond avec des montagnes"
-               className={styleBanner.banner__img}
+               className={styles.banner__img}
             />
             <h2 className={styles.banner__title}>
                Chez vous, <br className={styles.breakMobile} />
